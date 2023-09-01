d3b632c4ed9fd6e89b421ed4f68595689c0712a6
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-09-01 21:34:18 +0700

    link to product detail

--- src/customer/Components/Home/ProductInfoSection.jsx
+++ src/customer/Components/Home/ProductInfoSection.jsx
@@ -1,21 +1,26 @@
-
 import React from "react";
 import "./ProductInfoSection.css";
+import { Link } from "react-router-dom";
 
 const ProductInfoSection = () => {
   return (
     <div className="product-card">
       <div className="product-info-section">
         <div className="product-image ml-10">
-          <img 
-            src="https://classic.cdn.media.amplience.net/i/hibbett/1985-Air%20Jordan%201.png" 
-            alt="Product"
-            
+          <Link to="/product/52">
+            <img
+              src="https://classic.cdn.media.amplience.net/i/hibbett/1985-Air%20Jordan%201.png"
+              alt="Product"
             />
+          </Link>
         </div>
         <div className="product-info mb-10 -mt-10">
           <h2 className="text-lg">Air Jordan 1 Mid</h2>
-          <p className="mr-16 text-justify opacity-70 mt-7">Inspired by the original AJ1, this mid-top edition maintains the iconic look you love while choice colours and crisp leather give it a distinct identity</p>
+          <p className="mr-16 text-justify opacity-70 mt-7">
+            Inspired by the original AJ1, this mid-top edition maintains the
+            iconic look you love while choice colours and crisp leather give it
+            a distinct identity
+          </p>
         </div>
       </div>
     </div>
