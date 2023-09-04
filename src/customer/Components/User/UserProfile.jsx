47fe42aeee340a161c299ae5a414500bd05ecd5d
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-09-04 17:17:20 +0700

    display profile

--- src/customer/Components/User/UserProfile.jsx
+++ src/customer/Components/User/UserProfile.jsx
@@ -1,54 +1,152 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Typography } from "@mui/material";
+import {
+  Grid,
+  TextField,
+  Button,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@mui/material";
+import { Fragment } from "react";
 
 const UserProfile = () => {
+  const [user, setUser] = useState(null);
+  const jwt = localStorage.getItem("jwt");
 
-    const [user, setUser] = useState(null);
-    const jwt = localStorage.getItem('jwt');
-
-    useEffect(() => {
-        if (jwt) {
-          // Nếu có mã JWT, thêm nó vào tiêu đề Authorization
-          axios.get('http://localhost:5454/api/users/profile', {
-            headers: {
-              Authorization: `Bearer ${jwt}`,
-            },
-          })
-            .then((response) => {
-              setUser(response.data);
-            })
-            .catch((error) => {
-              console.error('Lỗi khi gọi API:', error);
-            });
-        }
-      }, [jwt]);
-
-    if (user === null){
-        return <div>Loading ...</div>
+  useEffect(() => {
+    if (jwt) {
+      axios
+        .get("http://localhost:5454/api/users/profile", {
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+          },
+        })
+        .then((response) => {
+          setUser(response.data);
+        })
+        .catch((error) => {
+          console.error("Lỗi khi gọi API:", error);
+        });
     }
+  }, [jwt]);
 
-    const address = user.addresses && user.addresses.length > 0 ? user.addresses[0] : null;
+  if (user === null) {
+    return <div>Loading ...</div>;
+  }
 
-    return (
-      <div>
-        <h1>User Profile</h1>
-        <p>First Name: {user.firstName}</p>
-        <p>Last Name: {user.lastName}</p>
-        <p>Email: {user.email}</p>
-        {address ? (
-          <div>
-            <h2>Address</h2>
-            <p>Street Address: {address.streetAddress}</p>
-            <p>City: {address.city}</p>
-            <p>State: {address.state}</p>
-            <p>Zip Code: {address.zipCode}</p>
-            <p>Mobile: {address.mobile}</p>
-          </div>
-        ) : (
-          <p>No address information available.</p>
-        )}
-      </div>
-  )
-}
+  const address =
+    user.addresses && user.addresses.length > 0 ? user.addresses[0] : null;
 
-export default UserProfile
+  return (
+    <Fragment className="createProductContainer ">
+      <Typography
+        variant="h3"
+        sx={{ textAlign: "center" }}
+        className="py-10 text-center "
+      >
+        Profile
+      </Typography>
+      <form
+        // onSubmit={handleSubmit}
+        className="createProductContainer min-h-screen"
+      >
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+            //   label="Email"
+              name="email"
+              value={user.email}
+            //   onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+            //   label="Brand"
+              name="firstName"
+              value={user.firstName}
+            //   onChange={handleChange}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+            //   label="Title"
+              name="lastName"
+              value={user.lastName}
+            //   onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+            //   label="Color"
+              name="street"
+              value={address.streetAddress}
+            //   onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+            //   label="Quantity"
+              name="state"
+              value={address.state}
+            //   onChange={handleChange}
+              
+            />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <TextField
+              fullWidth
+            //   label="Price"
+              name="city"
+              value={address.city}
+            //   onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <TextField
+              fullWidth
+            //   label="Discounted Price"
+              name="zipCode"
+              value={address.zipCode}
+            //   onChange={handleChange}
+            //   type="number"
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={4}>
+            <TextField
+              fullWidth
+            //   label="Discount Percentage"
+              name="mobile"
+              value={address.mobile}
+            //   onChange={handleChange}
+              type="number"
+            />
+          </Grid>
+    
+          <Grid item xs={12}>
+            <Button
+              variant="contained"
+              sx={{ p: 1.8 }}
+              className="py-20"
+              size="large"
+              type="submit"
+            >
+              Update Profile
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Fragment>
+  );
+};
+
+export default UserProfile;
