07bbf0e23aea09a327c02b1d2b3869d17f63d04f
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-09-07 00:02:41 +0700

    OK order

--- src/customer/Components/orders/OrderCard.jsx
+++ src/customer/Components/orders/OrderCard.jsx
@@ -119,6 +119,7 @@ const OrderCard = ({ item, order }) => {
           <p className="font-semibold" style={{ color: "red" }}> ${order?.totalDiscountedPrice}</p>  
       </div>
       <div className="flex justify-end items-center">
+        <p className="centered-text text-sm">Vui lòng chỉ nhấn "Đã nhận được hàng" khi đơn hàng đã được giao đến bạn và sản phẩm nhận được không có vấn đề nào</p>
         <button className="square-button btn-submit" >ĐÃ NHẬN HÀNG</button>
         <button className="square-button btn-contact" >LIÊN HỆ NGƯỜI BÁN</button>
       </div>
