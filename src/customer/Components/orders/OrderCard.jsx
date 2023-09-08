65e78e93a155196cbaa92689e2334070d3f0beba
tranthuyvy <ttv.thuyvy.1544@gmail.com>
2023-09-08 17:45:14 +0700

    npm i date-fns for format date

--- src/customer/Components/orders/OrderCard.jsx
+++ src/customer/Components/orders/OrderCard.jsx
@@ -4,8 +4,9 @@ import AdjustIcon from "@mui/icons-material/Adjust";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import StarIcon from "@mui/icons-material/Star";
-import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
 import "./OrderCard.css";
+import { format } from "date-fns";
 
 const OrderCard = ({ item, order }) => {
   const navigate = useNavigate();
@@ -20,6 +21,55 @@ const OrderCard = ({ item, order }) => {
 
   return (
     <Box className="p-5 shadow-lg hover:shadow-2xl border ">
+      <Grid
+        item
+        xs={12}
+        container
+        alignItems="center"
+        justifyContent="flex-end"
+      >
+        <p className="opacity-50 mr-5 text-sm">
+          Create At: {format(new Date(order?.createdAt), "dd/MM/yyyy")}{" "}
+        </p>
+        <p className="space-y-2 font-semibold">
+          <div style={{ display: "flex", alignItems: "center" }}>
+            {order?.orderStatus === "DELIVERED" ? (
+              <>
+                <FiberManualRecordIcon
+                  sx={{ width: "15px", height: "15px" }}
+                  className="text-green-600 p-0 mr-2 text-sm"
+                />
+                <span></span>
+              </>
+            ) : (
+              <>
+                <AdjustIcon
+                  sx={{ width: "15px", height: "15px" }}
+                  className="text-green-600 p-0 mr-2 text-sm"
+                />
+                <span></span>
+              </>
+            )}
+            <p className="mr-2" style={{ color: "green" }}>
+              <LocalShippingIcon />{" "}
+            </p>
+            <p className="font-semibold">{order?.orderStatus}</p>
+          </div>
+        </p>
+        <p className="text-xs"></p>
+        {item.orderStatus === "DELIVERED" && (
+          <div
+            onClick={() => navigate(`/account/rate/{id}`)}
+            className="flex items-center text-blue-600 cursor-pointer"
+          >
+            <StarIcon sx={{ fontSize: "2rem" }} className="px-2 text-5xl" />
+            <span>Rate & Review Product</span>
+          </div>
+        )}
+      </Grid>
+
+      <hr className="mt-5 mb-5" />
+
       <Grid spacing={2} container sx={{ justifyContent: "space-between" }}>
         <Grid item xs={5}>
           <div
@@ -27,12 +77,14 @@ const OrderCard = ({ item, order }) => {
             className="flex cursor-pointer"
           >
             <img
-              className="w-[5rem] h-[5rem] object-cover object-top"
+              className="w-[10rem] h-[10rem] object-cover object-top"
               src={item?.product.imageUrl}
               alt=""
             />
             <div className="ml-5">
-              <p className="mb-2 font-semibold">{item?.product.title}</p>
+              <p className="mb-2 font-semibold text-lg">
+                {item?.product.title}
+              </p>
               <p className="opacity-50 text-xs font-semibold space-x-5">
                 <span>
                   {item?.product.color}, {item?.size}
@@ -58,41 +110,47 @@ const OrderCard = ({ item, order }) => {
         </Grid>
 
         <Grid item xs={3} container alignItems="center">
-        
           <p style={{ color: "black", opacity: "50%", marginRight: "4px" }}>
             Item Price:{" "}
           </p>
-          <p className="font-semibold mr-2 ml-1" style={{ color: "gray", textDecoration: "line-through" }}> ${item?.product.price}</p>
-          <p className="font-semibold" style={{ color: "red" }}> ${item?.product.discountedPrice}</p>  
+          <p
+            className="font-semibold mr-2 ml-1"
+            style={{ color: "gray", textDecoration: "line-through" }}
+          >
+            {" "}
+            ${item?.product.price}
+          </p>
+          <p className="font-semibold" style={{ color: "red" }}>
+            {" "}
+            ${item?.product.discountedPrice}
+          </p>
         </Grid>
 
         <Grid item xs={2} container alignItems="center">
           <p className="space-y-2 font-semibold">
             <div style={{ display: "flex", alignItems: "center" }}>
-              
-            {order?.orderStatus === "DELIVERED" ? (
-              <>
-                <FiberManualRecordIcon
-                  sx={{ width: "15px", height: "15px" }}
-                  className="text-green-600 p-0 mr-2 text-sm"
-                />
-                <span></span>
-              </>
-            ) : (
-              <>
-                <AdjustIcon
-                  sx={{ width: "15px", height: "15px" }}
-                  className="text-green-600 p-0 mr-2 text-sm"
-                />
-                <span></span>
-              </>
-            )}
-              <p className="mr-2" style={{color:"green"}}><LocalShippingIcon/> </p>
+              {order?.orderStatus === "DELIVERED" ? (
+                <>
+                  <FiberManualRecordIcon
+                    sx={{ width: "15px", height: "15px" }}
+                    className="text-green-600 p-0 mr-2 text-sm"
+                  />
+                  <span></span>
+                </>
+              ) : (
+                <>
+                  <AdjustIcon
+                    sx={{ width: "15px", height: "15px" }}
+                    className="text-green-600 p-0 mr-2 text-sm"
+                  />
+                  <span></span>
+                </>
+              )}
+              <p className="mr-2" style={{ color: "green" }}>
+                <LocalShippingIcon />{" "}
+              </p>
               <p className="font-semibold">{order?.orderStatus}</p>
-              
             </div>
-            
-            
           </p>
           <p className="text-xs"></p>
           {item.orderStatus === "DELIVERED" && (
@@ -106,31 +164,39 @@ const OrderCard = ({ item, order }) => {
           )}
         </Grid>
       </Grid>
-      <hr className="mt-5"/>
+      <hr className="mt-5" />
       <div className="flex justify-end items-center mt-5">
         <img
           className="w-14 h-14 mr-2"
           src="https://cdn.printgo.vn/uploads/media/774255/logo-giay-1_1586510617.jpg"
         />
-          <p style={{ color: "black", opacity: "50%", marginRight: "4px" }}>
-            Total:{" "}
-          </p>
-          <p className="font-semibold mr-2 ml-1" style={{ color: "gray", textDecoration: "line-through" }}> ${totalPrice}</p>
-          <p className="font-semibold" style={{ color: "red" }}> ${order?.totalDiscountedPrice}</p>  
+        <p style={{ color: "black", opacity: "50%", marginRight: "4px" }}>
+          Total:{" "}
+        </p>
+        <p
+          className="font-semibold mr-2 ml-1"
+          style={{ color: "gray", textDecoration: "line-through" }}
+        >
+          {" "}
+          ${totalPrice}
+        </p>
+        <p className="font-semibold" style={{ color: "red" }}>
+          {" "}
+          ${order?.totalDiscountedPrice}
+        </p>
       </div>
       <div className="flex justify-end items-center">
-        <p className="centered-text text-sm">Vui lòng chỉ nhấn "Đã nhận được hàng" khi đơn hàng đã được giao đến bạn và sản phẩm nhận được không có vấn đề nào</p>
+        <p className="centered-text text-sm">
+          Vui lòng chỉ nhấn "Đã nhận được hàng" khi đơn hàng đã được giao đến
+          bạn và sản phẩm nhận được không có vấn đề nào
+        </p>
 
-        {order?.orderStatus === 'DELIVERED' ? (
-
-        <button className="square-button btn-submit">
-          ĐÃ NHẬN HÀNG
-        
-        </button>
+        {order?.orderStatus === "DELIVERED" ? (
+          <button className="square-button btn-submit">ĐÃ NHẬN HÀNG</button>
         ) : (
-          <button className="square-button-unsub" >ĐÃ NHẬN HÀNG</button>
+          <button className="square-button-unsub">ĐÃ NHẬN HÀNG</button>
         )}
-        <button className="square-button btn-contact" >LIÊN HỆ NGƯỜI BÁN</button>
+        <button className="square-button btn-contact">LIÊN HỆ NGƯỜI BÁN</button>
       </div>
     </Box>
   );
